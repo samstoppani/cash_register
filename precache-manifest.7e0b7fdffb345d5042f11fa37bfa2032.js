bd0eb2695a1a98e878878f67554dd006f8e98a07
Sam Stoppani <samstoppani@gmail.com>
2020-03-04 19:06:26 +0000

    Updates

---
+++ precache-manifest.7e0b7fdffb345d5042f11fa37bfa2032.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fddee9b77064125a214d9a2bb23f2e",
+    "url": "/cash_register/index.html"
+  },
+  {
+    "revision": "d255ad0eecd742755f80",
+    "url": "/cash_register/static/css/2.10111ff5.chunk.css"
+  },
+  {
+    "revision": "357041d9d25e11696ab9",
+    "url": "/cash_register/static/css/main.0d0a6967.chunk.css"
+  },
+  {
+    "revision": "d255ad0eecd742755f80",
+    "url": "/cash_register/static/js/2.75550abf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cash_register/static/js/2.75550abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357041d9d25e11696ab9",
+    "url": "/cash_register/static/js/main.831b382f.chunk.js"
+  },
+  {
+    "revision": "654f0f284b809e91001e",
+    "url": "/cash_register/static/js/runtime-main.d1dc8268.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cash_register/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cash_register/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cash_register/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cash_register/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cash_register/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
